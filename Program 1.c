3ad709e5a096761c633b7d8a45673796c08f87a7
ageementz <bes.tongtrongsub@gmail.com>
2018-03-26 17:44:58 +0700

    Modified Program 1.c

---
+++ Program 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+	{
+	char answer[64];
+
+	printf("TODAY IS MONDAY");
+	printf("QUESTION: HOW ARE YOU DOING TODAY?");
+	printf("ANSWER: ");
+	scanf("%s", &answer);
+	}
